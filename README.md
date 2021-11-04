771110286c1f57cec71c20c759beeb4beda07821
Shreejan Joshi <shreejanjoshi97@gmail.com>
2021-11-04 07:01:12 +0100

    added css, edit readme, made button

--- README.md
+++ README.md
@@ -1,7 +1,13 @@
 # lerning-with-mySQL
 
-### What's happeing
+### What's the Goal
 Here I'am trying to lern littel bit op MySQL.
 - How to connect php with SQL
 - make a new data 
 - connect that data to SQL
+
+### What's happing
+
+- I made a simple layout onlu username and password
+- used a bootstrap to make responsive
+
--- login.php
+++ login.php
@@ -7,21 +7,24 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/styles.css">
 </head>
 
 <body>
     <form action="">
         <div class="container">
             <div class="col-xs-6">
-                <form action="" method="">
+                <form action="login.php" method="post">
                     <div class="form-group">
                         <label for="username">Username</label>
                         <input type="text" class="form-control" name="usernamer">
                     </div>
                     <div class="form-group">
-                        <label for=""></label>
+                        <label for="password">Password</label>
                         <input type="password" class="form-control" name="password">
                     </div>
+
+                    <input class="btn btn-primary" type="submit" name="submit" value="Enter">
                 </form>
             </div>
         </div>
