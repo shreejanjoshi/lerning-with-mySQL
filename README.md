69d55eafc870a6247fbef9a2a7868c2178d49e80
Shreejan Joshi <shreejanjoshi97@gmail.com>
2021-11-04 06:42:00 +0100

    made a layout for my page

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # lerning-with-mySQL
+
+### What's happeing
+Here I'am trying to lern littel bit op MySQL.
+- How to connect php with SQL
+- make a new data 
+- connect that data to SQL
---
+++ login.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+</head>
+
+<body>
+    <form action="">
+        <div class="container">
+            <div class="col-xs-6">
+                <form action="" method="">
+                    <div class="form-group">
+                        <label for="username">Username</label>
+                        <input type="text" class="form-control" name="usernamer">
+                    </div>
+                    <div class="form-group">
+                        <label for=""></label>
+                        <input type="password" class="form-control" name="password">
+                    </div>
+                </form>
+            </div>
+        </div>
+    </form>
+</body>
+
+</html>
